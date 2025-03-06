f34566e4d7c4651a29dc1e4fc4efe6717b3b9d4c
Arshir <send2fareed@gmail.com>
2025-03-06 18:09:13 +0000

    commit changes in models and server file

--- BackEnd/part3/phonebook/models/phonebook.js
+++ BackEnd/part3/phonebook/models/phonebook.js
@@ -1,43 +1,41 @@
-const { default: mongoose } = require("mongoose");
-const { validate } = require("uuid");
+const { default: mongoose } = require('mongoose')
+//const {  } = require('uuid')
 
 mongoose.set('strictQuery',false)
 
 const url = process.env.MONGODB_URL
 
 mongoose.connect(url).
-then(success=> console.log('MongoDB connection succeeded')).
-catch(error=>{
+  then(() => console.log('MongoDB connection succeeded')).
+  catch(error => {
     console.log('Failure establishing connection to MONGODB',error.message)
-})
+  })
 
-const contactSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            minLength:5
-        },
-        
-        number: {
-            type: String,
-            validate: {
+const contactSchema = mongoose.Schema({
+  name: {
+    type: String,
+    minLength:5
+  },
 
-                validator: function(v){
-                    return /^(\d{2,3}-\d{5,})$/.test(v);
-                },
-                message: props=>`${props.value} is not a valid phone number!`
-            },
-            required:[true,'User phone number required']
-        }
-     }
+  number: {
+    type: String,
+    validate: {
+      validator: function(v){
+        return /^(\d{2,3}-\d{5,})$/.test(v)
+      },
+      message: props => `${ props.value} is not a valid phone number!`
+    },
+    required:[true,'User phone number required']
+  }
+}
 )
 
 contactSchema.set('toJSON',{
-    transform: (document, returnedDocument)=>{
-        returnedDocument.id = returnedDocument._id.toString()
-        delete returnedDocument._id
-        delete returnedDocument.__v
-    }
+  transform: (document, returnedDocument) => {
+    returnedDocument.id = returnedDocument._id.toString()
+    delete returnedDocument._id
+    delete returnedDocument.__v
+  }
 })
 
 /*contactSchema.set('toJSON', {
@@ -49,7 +47,7 @@ contactSchema.set('toJSON',{
   })*/
 
 
- module.exports = mongoose.model('contactModel',contactSchema)
+module.exports = mongoose.model('contactModel',contactSchema)
 
 
 
