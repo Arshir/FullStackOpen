f07bd68039997f86cfb61efef2d632a185a3acef
Arshir <send2fareed@gmail.com>
2025-03-20 18:09:01 +0000

    written code for the exercise 4.4: Helper Functions and Unit Tests, step 2

--- BackEnd/part4/utils/list_helper.js
+++ BackEnd/part4/utils/list_helper.js
@@ -3,4 +3,25 @@ const dummy=(blogs)=>{
 return 1
 }
 
-module.exports= dummy
+
+
+const getTotalLikes=(blogs)=>{
+
+    let totalLikes=0
+
+    totalLikes= blogs.reduce( (sum,item)=> sum + item.likes, 0)
+
+   /* if(blogs && blogs.Length>0)
+    {
+       totalLikes= blogs.reduce( (sum,item)=> sum + item.likes, 0)
+    }
+       */
+
+    return totalLikes
+
+}
+
+
+module.exports= {
+    dummy, getTotalLikes
+}
